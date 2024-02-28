611041cde4fbec1e82c97f4a8bdae0ddb09dfd48
trungpd1102 <156633588+trungphammycolor@users.noreply.github.com>
2024-02-28 10:43:23 +0700

    :sparkles: add build config

--- worker-nextjs/next.config.mjs
+++ worker-nextjs/next.config.mjs
@@ -4,10 +4,12 @@ import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 // (when running the application with `next dev`), for more information see:
 // https://github.com/cloudflare/next-on-pages/blob/5712c57ea7/internal-packages/next-dev/README.md
 if (process.env.NODE_ENV === 'development') {
-  await setupDevPlatform();
+	await setupDevPlatform();
 }
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	output: 'export',
+};
 
 export default nextConfig;
